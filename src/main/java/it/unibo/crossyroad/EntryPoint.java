75f50a1fc1811cac12c33ecdfb2e909dcb51dcbb
hider <alessio5.magnani@gmail.com>
2026-01-19 16:37:40 +0100

    initialize JavaFX application structure

---
+++ src/main/java/it/unibo/crossyroad/EntryPoint.java
@@ -0,0 +1,30 @@
+package it.unibo.crossyroad;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Entry point of the application. It initializes the MVC components and starts the JavaFX application.
+ */
+public class EntryPoint extends Application {
+    /**
+     * It initializes the MVC components.
+     *
+     * @throws Exception if any error occurs.
+     */
+    @Override
+    public void init() throws Exception {
+        // todo: Create instances and assign them to private fields
+    }
+
+    /**
+     * It links the MVC components and starts the JavaFX application.
+     *
+     * @param stage Stage of the application.
+     * @throws Exception if any error occurs.
+     */
+    @Override
+    public void start(final Stage stage) throws Exception {
+        // todo: Link instances of controller, view and model (that are in private fields)
+    }
+}
