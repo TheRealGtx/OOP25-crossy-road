75e6e77c71b94607551219514df6d3019ec20f65
Alessio Magnani <alessio.magnani@studio.unibo.it>
2026-01-31 22:25:48 +0100

    add River chunk, that generates Water and WoodLog obstacles

---
+++ src/main/java/it/unibo/crossyroad/model/impl/River.java
@@ -0,0 +1,104 @@
+package it.unibo.crossyroad.model.impl;
+
+import it.unibo.crossyroad.model.api.AbstractActiveChunk;
+import it.unibo.crossyroad.model.api.ActiveObstacle;
+import it.unibo.crossyroad.model.api.Dimension;
+import it.unibo.crossyroad.model.api.Direction;
+import it.unibo.crossyroad.model.api.EntityType;
+import it.unibo.crossyroad.model.api.Obstacle;
+import it.unibo.crossyroad.model.api.Position;
+
+/**
+ * Class representing a River chunk in the game.
+ */
+public class River extends AbstractActiveChunk {
+    private static final int LOGS_DISTANCE = 3;
+    private static final double LOGS_SPEED = 1.0;
+    private static final int LOGS_LENGTH = 4;
+
+    private final Direction direction;
+    private final long logInterval;
+    private long timeSinceLastLog;
+
+    /**
+     * Constructor for River.
+     *
+     * @param initialPosition The initial position of the River chunk.
+     * @param dimension The dimension of the River chunk.
+     * @param direction The direction of the logs in the river.
+     */
+    public River(final Position initialPosition, final Dimension dimension, final Direction direction) {
+        super(initialPosition, dimension);
+
+        if (direction != Direction.LEFT && direction != Direction.RIGHT) {
+            throw new IllegalArgumentException("Direction must be LEFT or RIGHT for River logs.");
+        }
+
+        this.direction = direction;
+        this.logInterval = (long) (LOGS_DISTANCE / LOGS_SPEED * 1000);
+        this.timeSinceLastLog = 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void init() {
+        this.addWater();
+        this.generateObstacles();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected boolean shouldGenerateNewObstacles(final long deltaTime) {
+        this.timeSinceLastLog += deltaTime;
+
+        if (this.timeSinceLastLog >= this.logInterval) {
+            this.timeSinceLastLog = 0;
+            return true;
+        }
+        return false;
+    }
+
+    private void addWater() {
+        final Obstacle water = new Water(
+            new Position(this.getPosition().x(), this.getPosition().y()),
+            new Dimension(this.getDimension().width(), this.getDimension().height())
+        );
+
+        this.addObstacle(water);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void generateObstacles() {
+        final ActiveObstacle log = new WoodLog(
+            LOGS_SPEED,
+            this.direction,
+            this.getLogStartPosition(),
+            LOGS_LENGTH
+        );
+
+        this.addObstacle(log);
+    }
+
+    private Position getLogStartPosition() {
+        return switch (this.direction) {
+            case LEFT -> new Position(this.getPosition().x() + this.getDimension().width(), this.getPosition().y());
+            case RIGHT -> new Position(this.getPosition().x() - LOGS_LENGTH, this.getPosition().y());
+            default -> throw new IllegalStateException("Direction must be LEFT or RIGHT");
+        };
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public EntityType getEntityType() {
+        return EntityType.RIVER;
+    }
+}
---
+++ src/main/java/it/unibo/crossyroad/model/impl/WoodLog.java
@@ -0,0 +1,51 @@
+package it.unibo.crossyroad.model.impl;
+
+import it.unibo.crossyroad.model.api.AbstractActiveObstacle;
+import it.unibo.crossyroad.model.api.CollisionType;
+import it.unibo.crossyroad.model.api.Direction;
+import it.unibo.crossyroad.model.api.GameParameters;
+import it.unibo.crossyroad.model.api.Position;
+import it.unibo.crossyroad.model.api.Dimension;
+import it.unibo.crossyroad.model.api.EntityType;
+
+/**
+ * A class representing a wood log transporting obstacle in the game.
+ */
+public class WoodLog extends AbstractActiveObstacle {
+
+    /**
+     * Constructor for WoodLog.
+     *
+     * @param speed Speed of the log.
+     * @param direction Direction of the log.
+     * @param position Initial position of the log
+     * @param length Length of the log.
+     */
+    public WoodLog(final double speed, final Direction direction, final Position position, final double length) {
+        super(position, new Dimension(length, 1), speed, direction);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public CollisionType getCollisionType() {
+        return CollisionType.TRANSPORT;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public EntityType getEntityType() {
+        return EntityType.WOOD_LOG;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected double getSpeedMultiplier(final GameParameters parameters) {
+        return 1.0;  // TODO
+    }
+}
