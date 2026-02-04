7c60017fcaadee63d1fb2ec90e861a74fc3d6cec
Mattia Golinucci <mattia.golinucci2@studio.unibo.it>
2026-02-04 18:04:08 +0100

    fix updatePowerUpTime and show

--- src/main/java/it/unibo/crossyroad/view/impl/GameViewImpl.java
+++ src/main/java/it/unibo/crossyroad/view/impl/GameViewImpl.java
@@ -4,13 +4,12 @@ import it.unibo.crossyroad.controller.api.GameController;
 import it.unibo.crossyroad.model.api.EntityType;
 import it.unibo.crossyroad.model.api.Positionable;
 import it.unibo.crossyroad.view.api.GameView;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.util.Duration;
 
 import java.util.EnumMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -18,14 +17,12 @@ import java.util.Map;
  * Implementation of the GameView interface.
  *
  */
-public class GameViewImpl implements GameView {
+public class GameViewImpl extends StackPane implements GameView {
 
-    private final Map<EntityType, Label> powerUpLabels;
     private final VBox powerUpBox = new VBox(5);
     private final Label coinLabel = new Label();
 
     public GameViewImpl() {
-        this.powerUpLabels = new EnumMap<>(EntityType.class);
     }
 
     @Override
@@ -45,18 +42,10 @@ public class GameViewImpl implements GameView {
     public void updatePowerUpTime(Map<EntityType, Long> powerUps) {
         powerUpBox.getChildren().clear();
         for (Map.Entry<EntityType, Long> entry: powerUps.entrySet()) {
-            EntityType powerUpType = entry.getKey();
             final int duration = (int) (entry.getValue() / 1000);
-
-            if (powerUpLabels.containsKey(powerUpType)) {
-
-                startPowerUpTimer(powerUpType, duration);
-            } else {
-                final Label label = new Label(formatPowerUpText(powerUpType, duration));
-                powerUpLabels.put(powerUpType, label);
-                powerUpBox.getChildren().add(label);
-                startPowerUpTimer(powerUpType, duration);
-            }
+            powerUpBox.getChildren().add(new Label(
+                    formatPowerUpText(entry.getKey(), duration)
+            ));
         }
     }
 
@@ -72,16 +61,6 @@ public class GameViewImpl implements GameView {
     }
 
     /**
-     * Extracts the remaining time from the label text.
-     *
-     * @param label the label containing the time
-     * @return the remaining time in seconds
-     */
-    private int extractTime(Label label) {
-        return Integer.parseInt(label.getText().replace("\\D+", ""));
-    }
-
-    /**
      * {@inheritDoc}
      */
     @Override
@@ -91,7 +70,7 @@ public class GameViewImpl implements GameView {
 
     @Override
     public void show() {
-        //TODO: Mattia
+        this.setVisible(true);
     }
 
     @Override
